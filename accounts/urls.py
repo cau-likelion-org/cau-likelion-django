2ef3b2ca54fe9c8932ab84cedf676feae8ec26a1
hayeongKo <polarpheno@icloud.com>
2023-02-18 04:11:45 +0900

    [MODIFY] 인가코드 수정 완료

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('google/login/', google_callback, name='google_callback'),
+    path('google/callback/', google_callback, name='google_callback'),
     path('google/login/finish/', GoogleLogin.as_view(), name='google_login_todjango'),
     path('authentication/', cau_authentication, name='school_email_authentiation'),
     path('caumail/', CauMailView.as_view()),
