323eb96c1e6dad487d9b6e740f4ed45ab4991188
sueun lee <1015_s@naver.com>
2023-03-05 17:48:39 +0900

    [MODIFY] attendance check 오류 해결

--- auths/urls.py
+++ auths/urls.py
@@ -3,6 +3,6 @@ from .views import *
 from rest_framework_simplejwt.views import (TokenObtainPairView, TokenRefreshView)
 
 urlpatterns = [
-    #path('', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('/token', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('', TokenRefreshView.as_view(), name='token_refresh'),
 ]
