be8cd12ef38b21ac75ca3bc8e1914206a17bc9e0
Kim mingyu <minq0302@naver.com>
2023-03-19 15:39:50 +0900

    아카이빙 post 구현 완료

--- lectures/views.py
+++ lectures/views.py
@@ -1,16 +1,24 @@
 # 데이터 처리
-from .models import Session
+from .models import Session, SessionImage
+from accounts.models import User
 from .serializers import SessionSerializer
-from rest_framework.decorators import action
 from rest_framework.response import Response
 from rest_framework.views import APIView
-from rest_framework import status, viewsets
+from rest_framework import status
 from django.http import Http404
+from config.settings import AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY
+import boto3
 
 # 세션의 목록을 보여주는 역할
 class SessionList(APIView):
+    s3_client = boto3.client(
+            's3',
+            aws_access_key_id = AWS_ACCESS_KEY_ID,
+            aws_secret_access_key = AWS_SECRET_ACCESS_KEY,
+        )
     # 세션 리스트를 보여줄 때
     def get(self,request):
+
         session_list = Session.objects.values()
     
         pm = []
@@ -67,6 +75,70 @@ class SessionList(APIView):
                 "4" : etc,
             }
         }, status=status.HTTP_200_OK)
+    
+    def post(self, request):
+        title = request.POST['title']
+        track = request.POST['track']
+        thumbnail = request.FILES['thumbnail']
+        presenter = request.POST['presenter']
+        date = request.POST['date']
+        description = request.POST['description']
+        topic = request.POST['topic']
+        reference = request.POST['reference']
+        degree = request.POST['degree']
+        login_email = request.POST['login_email']
+
+        memberid = User.objects.get(
+            email = login_email
+        )
+
+        thumbnail_url = f"sessions/{title}/thumbnail" # DB에 저장될 썸네일 이미지 url 설정
+        self.s3_client.upload_fileobj(
+            thumbnail,
+            "chunghaha",
+            thumbnail_url,
+            ExtraArgs={
+                    "ContentType": thumbnail.content_type
+                }
+        )
+        session_post = Session.objects.create(
+            title = title,
+            thumbnail = "https://dsu3068f46mzk.cloudfront.net/" + thumbnail_url,
+            date = date,
+            description = description,
+            track = track,
+            presenter = presenter,
+            topic = topic,
+            reference = reference,
+            degree = degree,
+            user_id = memberid
+        )
+        session_post.save()
+
+        images = request.FILES.getlist('images')
+        cnt = 1
+        for image in images:
+            image_url = f"sessions/{title}/image{cnt}"
+            self.s3_client.upload_fileobj(
+                image,
+                "chunghaha",
+                image_url,
+                ExtraArgs={
+                        "ContentType": image.content_type
+                    }
+            )
+            image = SessionImage.objects.create(
+                session = session_post,
+                image = "https://dsu3068f46mzk.cloudfront.net/" + image_url
+            )
+            cnt = cnt + 1
+
+        return Response(data={
+        "message" : "success",
+        "data" : {
+            "title" : title
+        }
+    }, status=status.HTTP_200_OK)
 
 # Session의 detail을 보여주는 역할
 class SessionDetail(APIView):
