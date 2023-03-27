3e2b703bf00d223ddc1365eed4191a7324020763
sueun lee <1015_s@naver.com>
2023-03-28 03:34:03 +0900

    [HOTFIX] logging 삭제

--- config/middleware/custom_middleware.py
+++ config/middleware/custom_middleware.py
@@ -3,14 +3,6 @@ import logging
 
 class SetDatabaseMiddleware(MiddlewareMixin):
     def process_request(self, request):
-        logger = logging.getLogger()
-        logger.setLevel(logging.INFO)
-    
-        logger.warning(request.META['HTTP_REFERER'])
-    
-        stream_handler = logging.StreamHandler()
-        logger.addHandler(stream_handler)
-        
         if 'HTTP_REFERER' in request.META:
             referer = request.META['HTTP_REFERER']
             if 'cau-likelion.org' in referer:
