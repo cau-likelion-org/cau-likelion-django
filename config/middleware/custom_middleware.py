99e7b3f805fc6d467c57be25291a4f8e237c83a7
sueun lee <1015_s@naver.com>
2023-03-28 03:49:05 +0900

    [HOTFIX] http_origin으로 변경

--- config/middleware/custom_middleware.py
+++ config/middleware/custom_middleware.py
@@ -3,10 +3,17 @@ import logging
 
 class SetDatabaseMiddleware(MiddlewareMixin):
     def process_request(self, request):
-        if 'HTTP_REFERER' in request.META:
-            referer = request.META['HTTP_REFERER']
-            if 'cau-likelion.org' in referer:
+        if 'HTTP_ORIGIN' in request.META:
+            origin = request.META['HTTP_ORIGIN']
+            
+            logger = logging.getLogger()
+            logger.setLevel(logging.INFO)
+            logger.warning(origin)
+            stream_handler = logging.StreamHandler()
+            logger.addHandler(stream_handler)
+            
+            if 'cau-likelion.org' in origin:
                 request.session['database'] = 'chunghaha'
-            elif 'dev.cau-likelion.org' in referer:
+            elif 'dev.cau-likelion.org' in origin:
                 request.session['database'] = 'chunghaha-dev'
         return None
