29ea33af5868e3ada03d6d73a37b16482adb05de
sueun lee <1015_s@naver.com>
2023-03-28 03:28:36 +0900

    [HOTFIX] logging

--- config/middleware/custom_middleware.py
+++ config/middleware/custom_middleware.py
@@ -1,24 +1,20 @@
+from django.utils.deprecation import MiddlewareMixin
 import logging
 
-class DatabaseMiddleware:
-    def __init__(self, get_response):
-        self.get_response = get_response
-
-    def __call__(self, request):
-        #logging
+class SetDatabaseMiddleware(MiddlewareMixin):
+    def process_request(self, request):
         logger = logging.getLogger()
         logger.setLevel(logging.INFO)
-        logger.warning(request.META['HTTP_HOST'])
+    
+        logger.warning(request.META['HTTP_REFERER'])
+    
         stream_handler = logging.StreamHandler()
         logger.addHandler(stream_handler)
         
-        if request.META['HTTP_HOST'] == 'cau-likelion.org' or 'api-cau-likelion.shop':
-            request.database = 'chunghaha'
-        elif request.META['HTTP_HOST'] == 'dev.cau-likelion.org':
-            request.database = 'chunghaha-dev'
-        else:
-            request.database = 'default'
-
-        response = self.get_response(request)
-
-        return response
+        if 'HTTP_REFERER' in request.META:
+            referer = request.META['HTTP_REFERER']
+            if 'cau-likelion.org' in referer:
+                request.session['database'] = 'chunghaha'
+            elif 'dev.cau-likelion.org' in referer:
+                request.session['database'] = 'chunghaha-dev'
+        return None
